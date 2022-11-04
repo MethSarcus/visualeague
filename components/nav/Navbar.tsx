f14dce59d20362b898643af35df21751487c2ded
Seth Marcus <seth@smarcus.dev>
2022-11-04 15:51:55 -0400

    Changed name

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -15,7 +15,7 @@ function Navbar() {
           </Box>
         <Box as={"b"} fontSize="lg">
           <Link href="/">
-            <a>Fanstatsy</a>
+            <a>Visualeague</a>
           </Link>
         </Box>
 
