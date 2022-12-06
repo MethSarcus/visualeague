5de661707f2b351c6a70e1ecd8c91ea413891b6b
Seth Marcus <seth@sethmarc.us>
2022-12-06 13:30:30 -0500

    Did some work on league stat group

--- components/groups/stats/LeagueWeekGroup.tsx
+++ components/groups/stats/LeagueWeekGroup.tsx
@@ -1,21 +1,41 @@
-import { Card, HStack } from "@chakra-ui/react";
+import { Card, HStack, Spinner } from "@chakra-ui/react";
 import League from "../../../classes/custom/League";
+import Matchup from "../../../classes/custom/Matchup";
+import { MatchupSide } from "../../../classes/custom/MatchupSide";
+import LeagueNotableWeekStatCard from "../../cards/statcards/LeagueNotableWeekStatCard";
 import WeekStatCard from "../../cards/statcards/WeekStatCard";
 
-
 interface MyProps {
-    league: League | undefined
+  league: League | undefined;
 }
 export default function LeagueNotableWeeksStatGroup(props: MyProps) {
+  let bestWeekTeam
+  let worstWeek
 
-    let notablePlayers
+  if (props.league?.settings != undefined) {
+    let notableWeeks = props.league.getLeagueNotableWeeks();
+    bestWeekTeam = (notableWeeks.matchupForBestTeam as unknown as Matchup).getWinner()
 
-    if (props.league?.settings) {
-        notablePlayers = props.league.getLeagueNotableWeeks()
-        console.log(notablePlayers)
-    }
+  }
 
-    return (<HStack>
-        nfd
-    </HStack>)
+  return (
+    <HStack>
+      <WeekStatCard
+        matchupSide={
+          bestWeekTeam}
+        memberId={
+          bestWeekTeam?.roster_id
+        }
+        mainStat={bestWeekTeam?.pf?.toFixed(2)}
+        title={"Best Weekly Score"}
+        isLoaded={bestWeekTeam != undefined}
+      />
+      {/* <LeagueNotableWeekStatCard
+        matchup={notableWeeks?.matchupForBestTeam}
+        title={undefined}
+        mainStat={undefined}
+        isLoaded={false}
+      /> */}
+    </HStack>
+  );
 }
