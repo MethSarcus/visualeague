53611bd371ac858f5307c36f70ced4f2baf1f6f8
Seth <sem4140@rit.edu>
2022-12-07 00:03:42 -0500

    took out uneeded routers

--- components/page_layouts/TradePageContent.tsx
+++ components/page_layouts/TradePageContent.tsx
@@ -10,7 +10,6 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 import { usePathname } from "next/navigation";
-import router, { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
 import useSWR from "swr";
 import League from "../../classes/custom/League";
