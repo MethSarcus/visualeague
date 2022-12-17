9ce7d4482f2d8ae252205f5f6b319d9088ffed87
Seth Marcus <seth@sethmarc.us>
2022-12-17 16:50:05 -0500

    Added skelies

--- components/cards/statcards/WeekStatCard.tsx
+++ components/cards/statcards/WeekStatCard.tsx
@@ -1,103 +1,107 @@
-"use client";
+'use client'
 import {
-  Avatar,
-  Box,
-  Button,
-  Center,
-  Flex,
-  Modal,
-  ModalOverlay,
-  Spacer,
-  Text,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { useContext } from "react";
-import LeagueMember from "../../../classes/custom/LeagueMember";
-import Matchup from "../../../classes/custom/Matchup";
-import { MatchupSide } from "../../../classes/custom/MatchupSide";
-import { Context } from "../../../contexts/Context";
-import MatchupModalBody from "../../MatchupModalBody";
+	Avatar,
+	Box,
+	Button,
+	Center,
+	Flex,
+	Modal,
+	ModalOverlay,
+	Skeleton,
+	SkeletonCircle,
+	Spacer,
+	Text,
+	useDisclosure,
+	VStack,
+} from '@chakra-ui/react'
+import {useContext} from 'react'
+import LeagueMember from '../../../classes/custom/LeagueMember'
+import Matchup from '../../../classes/custom/Matchup'
+import {MatchupSide} from '../../../classes/custom/MatchupSide'
+import {Context} from '../../../contexts/Context'
+import MatchupModalBody from '../../MatchupModalBody'
 
 type MyProps = {
-  matchupSide: MatchupSide | undefined;
-  memberId: number | undefined;
-  mainStat: String | undefined;
-  subStat?: String | undefined;
-  matchup?: Matchup | undefined;
-  title: String | undefined;
-  isLoaded: boolean;
-};
+	matchupSide: MatchupSide | undefined
+	memberId: number | undefined
+	mainStat: String | undefined
+	subStat?: String | undefined
+	matchup?: Matchup | undefined
+	title: String | undefined
+	isLoaded: boolean
+}
 
 const WeekStatCard = (props: MyProps) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [context, setContext] = useContext(Context);
+	const {isOpen, onOpen, onClose} = useDisclosure()
+	const [context, setContext] = useContext(Context)
 
-  let leagueMember;
+	let leagueMember
 
-  if (context?.settings != undefined) {
-    leagueMember = context?.members?.get(props.memberId) as LeagueMember;
-  }
-  return (
-    <Flex
-    minWidth={"150px"}
-    flexDir={"column"}
-      border={"1px"}
-      borderRadius={4}
-      boxShadow={"2xl"}
-      py={2}
-      px={5}
-      bg={"surface.0"}
-      dropShadow="2xl"
-    >
-      <Spacer/>
-      <Box textAlign={"center"}>
-        <Center flexDir={"column"}>
+	if (context?.settings != undefined) {
+		leagueMember = context?.members?.get(props.memberId) as LeagueMember
+	}
+	return (
+		<Flex
+			minWidth={'150px'}
+			border={'1px'}
+			borderRadius={4}
+			boxShadow={'2xl'}
+			bg={'surface.0'}
+			dropShadow='2xl'
+		>
+			<Box textAlign={'center'}>
+				<Skeleton isLoaded={props.isLoaded}>
+					<Text fontWeight='normal' fontSize={'1em'} color={'white'}>
+						{props.title}
+					</Text>
+				</Skeleton>
 
+				<VStack>
+					<SkeletonCircle isLoaded={props.isLoaded} mt={2}>
+						<Avatar
+							size={'md'}
+							src={`https://sleepercdn.com/avatars/thumbs/${leagueMember?.avatar}`}
+						/>
+					</SkeletonCircle>
+					<Skeleton isLoaded={props.isLoaded}>
+						<Text
+							fontSize={'.8em'}
+							fontWeight='medium'
+							color={'textTheme.highEmphasis'}
+						>
+							{props.subStat}
+						</Text>
+					</Skeleton>
+				</VStack>
 
-        <Box fontWeight="normal" fontSize={"1em"} color={"white"}>
-          {props.title}
-        </Box>
+				<Skeleton isLoaded={props.isLoaded}>
+					<Text
+						fontWeight={'bold'}
+						fontSize={'1.2em'}
+						color={'textTheme.highEmphasis'}
+					>
+						{props.mainStat}
+					</Text>
+				</Skeleton>
 
+				<Button
+					visibility={props.matchup != undefined ? 'visible' : 'hidden'}
+					mt={2}
+					isLoading={!props.isLoaded}
+					variant={'ghost'}
+					colorScheme={'secondary'}
+					onClick={onOpen}
+					size={'xs'}
+				>
+					View
+				</Button>
+				<Modal size={'sm'} isOpen={isOpen} onClose={onClose}>
+					<ModalOverlay />
+					<MatchupModalBody matchup={props.matchup} onClose={onClose} />
+				</Modal>
+			</Box>
+		</Flex>
+	)
+}
 
-        <Avatar
-          mt={2}
-          size={"md"}
-          src={`https://sleepercdn.com/avatars/thumbs/${leagueMember?.avatar}`}
-        />
-        <Text
-          fontSize={".8em"}
-          fontWeight="medium"
-          color={"textTheme.highEmphasis"}
-        >
-          {props.subStat}
-        </Text>
-        <Text
-          fontWeight={"bold"}
-          fontSize={"1.2em"}
-          color={"textTheme.highEmphasis"}
-        >
-          {props.mainStat}
-        </Text>
-        </Center>
-<Spacer/>
-        <Button
-          visibility={props.matchup != undefined ? "visible" : "hidden"}
-          mt={2}
-          variant={"ghost"}
-          colorScheme={"secondary"}
-          onClick={onOpen}
-          size={"xs"}
-        >
-          View
-        </Button>
-        <Modal size={"sm"} isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <MatchupModalBody matchup={props.matchup} onClose={onClose} />
-        </Modal>
-      </Box>
-      <Spacer/>
-    </Flex>
-  );
-};
-
-export default WeekStatCard;
+export default WeekStatCard
